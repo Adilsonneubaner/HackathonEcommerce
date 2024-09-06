2657be85d3b24fc67f82fd220a4161cb40caee03
Rayanne Ramos <rayanneramosoficial@gmail.com>
2024-09-06 19:01:39 -0300

    :sparkles: feat: create total price functionality

--- farma-tech/src/pages/Shopcart/index.jsx
+++ farma-tech/src/pages/Shopcart/index.jsx
@@ -2,31 +2,49 @@ import styles from "./shopcart.module.css";
 import Info from "../../components/Info/Info";
 import Footer from "../../components/Footer/Footer";
 import { ShopcartCard } from "../../components/ShopcartCard";
+import { useState } from "react";
 
 export function Shopcart() {
+  const [quantity, setQuantity] = useState(1);
+  const [productPrince] = useState(10.99);
+  const shippingCost = 3.9;
+  const totalPrice = (productPrince * quantity + shippingCost).toFixed(2);
+
+  function handleIncreaseQuantity() {
+    setQuantity((prevQuantity) => prevQuantity + 1);
+  }
+
+  function handleDecreaseQuantity() {
+    setQuantity((prevQuantity) => prevQuantity - 1);
+  }
+
   return (
     <div className={styles.container}>
       <Info />
       <strong className={styles.titlePage}>Sacola de compras</strong>
       <div className={styles.content}>
         <div className={styles.shopcartContainer}>
-          <ShopcartCard />
-          <ShopcartCard />
-          <ShopcartCard />
+          <ShopcartCard
+            quantity={quantity}
+            handleIncreaseQuantity={handleIncreaseQuantity}
+            handleDecreaseQuantity={handleDecreaseQuantity}
+          />
         </div>
         <div className={styles.finishBuyContainer}>
           <div className={styles.findBuyContent}>
             <div className={styles.findBuyQuantity}>
               <span>Quantidade</span>
-              <span>1 item</span>
+              <span>
+                {quantity} {quantity === 1 ? "item" : "itens"}
+              </span>
             </div>
             <div className={styles.findBuyShippingCost}>
               <span>Valor do Frete</span>
-              <span>R$ 3,90</span>
+              <span>R$ {shippingCost.toFixed(2)}</span>
             </div>
             <div className={styles.findBuyTotal}>
               <span>Valor Total</span>
-              <strong>R$ 79,90</strong>
+              <strong>R$ {totalPrice}</strong>
             </div>
             <button className={styles.findBuyButton}>Finalizar compra</button>
           </div>
